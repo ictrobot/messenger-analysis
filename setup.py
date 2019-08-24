f4bfae1a14688398f508e93d8e00c7dbf4b61a54
ictrobot <ictrobot@outlook.com>
2019-08-24 16:51:35 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='messenger-analysis',
+    version='0.0.1dev',
+    author="ictrobot",
+    author_email="ictrobot@outlook.com",
+    packages=['mda',],
+    license='MIT',
+    install_requires=["pytz"],
+)
